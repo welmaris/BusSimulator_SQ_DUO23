86c840e7e537d282c64dcd2e66f6bc52e1ae2e02
2zqa <25235249+2zqa@users.noreply.github.com>
2023-01-27 23:37:49 +0100

    Refactor Dashboard

--- Dashboard/dashboard/Dashboard.java
+++ Dashboard/dashboard/Dashboard.java
@@ -2,6 +2,8 @@ package Dashboard.dashboard;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -14,66 +16,83 @@ import javafx.stage.Stage;
 import DatabaseLogger.mockDatabaseLogger.ArrivaLogger;
 import InfobordSysteem.infoborden.Infobord;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 import Simulator.bussimulator.Runner;
 
 public class Dashboard extends Application {
-		
-    private void thread(Runnable runnable, boolean daemon) {
-		Thread brokerThread = new Thread(runnable);
-		brokerThread.setDaemon(daemon);
-		brokerThread.start();
-	}
-    
-    private void startBord(String halte, String richting) {
-		Infobord infobord = new Infobord(halte,richting);
-		Platform.runLater(new Runnable() {
-			public void run() {             
-				infobord.start(new Stage());
-			}
-		});	    	
-    }
-	private void startAlles() {
-		thread(new Runner(),false); 
-	}
 
-	@Override // Override the start method in the Application class
-	public void start(Stage primaryStage) {
-		// Create a pane and set its properties
-		GridPane pane = new GridPane();
-		pane.setAlignment(Pos.CENTER);
-		pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
-		pane.setHgap(5.5);
-		pane.setVgap(5.5);
-		pane.add(new Label("Halte:"), 0, 0);
-		TextField halteInput = new TextField("A-Z");
-		pane.add(halteInput, 1, 0);
-		pane.add(new Label("Richting:"), 0, 1);
-		TextField richting = new TextField("1 of -1");
-		pane.add(richting, 1, 1);
-		Button btBord = new Button("Start Bord");
-		btBord.setOnAction(e -> {
-			startBord(halteInput.getText(), richting.getText());
-		});
-		Button btStart = new Button("Start");
-		btStart.setOnAction( e -> {
-			startAlles();
-		});
-		Button btLogger = new Button("Start Logger");
-		btLogger.setOnAction( e -> {
-			thread(new ArrivaLogger(), false);
-		});
-		pane.add(btBord, 1, 5);
-		pane.add(btStart, 2, 5);
-		pane.add(btLogger, 3, 5);
-		GridPane.setHalignment(btBord, HPos.LEFT);
-		GridPane.setHalignment(btStart, HPos.LEFT);
-		GridPane.setHalignment(btLogger, HPos.LEFT);
-		Scene scene = new Scene(pane);
-		primaryStage.setTitle("BusSimulatie control-Center");
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
+    private void thread(Runnable runnable, boolean daemon) {
+        Thread brokerThread = new Thread(runnable);
+        brokerThread.setDaemon(daemon);
+        brokerThread.start();
+    }
+
+    private void startBord(String halte, String richting) {
+        Infobord infobord = new Infobord(halte, richting);
+        Platform.runLater(() -> infobord.start(new Stage()));
+    }
+
+    private void startAlles() {
+        thread(new Runner(), false);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        GridPane pane = createGridPane();
+
+        TextField halteInput = addTextField("Halte:", "A-Z", pane, 0);
+        TextField richtingInput = addTextField("Richting:", "1 of -1", pane, 1);
+
+        addButtons(pane, e -> startBord(halteInput.getText(), richtingInput.getText()));
+
+        showStage(primaryStage, pane, "BusSimulatie control-Center");
+    }
+
+    private void showStage(Stage primaryStage, GridPane pane, String title) {
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle(title);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private TextField addTextField(String label, String placeholder, GridPane pane, int index) {
+        pane.add(new Label(label), 0, index);
+        TextField textField = new TextField(placeholder);
+        pane.add(textField, 1, index);
+        return textField;
+    }
+
+    private void addButtons(GridPane pane, EventHandler<ActionEvent> startButtonEventHandler) {
+//    Button 1: start bord
+        Button startBordButton = createDashboardButton("Start Bord", startButtonEventHandler);
+        pane.add(startBordButton, 1, 5);
+
+//    Button 2: start
+        Button startButton = createDashboardButton("Start Alles", e -> startAlles());
+        pane.add(startButton, 2, 5);
+
+//    Button 3: start Logger
+        Button loggerButton = createDashboardButton("Start Logger", e -> thread(new ArrivaLogger(), false));
+        pane.add(loggerButton, 3, 5);
+    }
+
+    private GridPane createGridPane() {
+//    create Pane
+        GridPane pane = new GridPane();
+        pane.setAlignment(Pos.CENTER);
+        pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+        pane.setHgap(5.5);
+        pane.setVgap(5.5);
+        pane.setMinWidth(400);
+        pane.setMinHeight(150);
+
+        return pane;
+    }
+
+    private Button createDashboardButton(String label, EventHandler<ActionEvent> eventHandler) {
+        Button button = new Button(label);
+        button.setOnAction(eventHandler);
+        GridPane.setHalignment(button, HPos.LEFT);
+        return button;
+    }
+
 } 
